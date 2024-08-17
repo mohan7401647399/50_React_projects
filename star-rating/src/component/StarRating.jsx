1860c6a5fe422da0444f004f3f32ff4d10543c65
mohan7401647399 <mtm.kcs@gmail.com>
2024-08-17 12:19:42 +0530

    'star-rating'

---
+++ star-rating/src/component/StarRating.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./Styles.css";
+
+const StarRating = ({ noOfStars }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
